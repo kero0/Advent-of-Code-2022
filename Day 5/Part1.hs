197c59c9d163966125185830351b7cac50e62891
kero0 <kbakheat@gmail.com>
2022-12-06 09:36:03 -0500

    Day 5

---
+++ Day 5/Part1.hs
@@ -0,0 +1,33 @@
+import Data.Bifunctor (bimap)
+import Data.List.Split (chunksOf)
+import Data.Maybe (fromJust, isNothing)
+import Data.Text (Text, pack, strip, unpack)
+import Data.List (transpose)
+main :: IO ()
+main = do
+  txt <- readFile "data.txt"
+  print
+    $ map head
+    $ uncurry doMoves
+    $ bimap
+      ( map (map fromJust . dropWhile isNothing)
+          . transpose
+          . map (map (toletter . unpack . strip . pack) . chunksOf 4)
+          . init
+      )
+      (map ((\[_, num, _, from, _, to] -> (read num, read from - 1, read to - 1) :: (Int, Int, Int)) . words) . tail)
+    $ break (== "")
+    $ lines txt
+  where
+    toletter [_, c, _] = Just c
+    toletter _ = Nothing
+
+doMoves :: [String] -> [(Int, Int, Int)] -> [String]
+doMoves state [] = state
+doMoves state ((num, from, to) : rest) =
+  flip doMoves rest $
+    setAt from (drop num $ state !! from) $
+      setAt to (reverse (take num (state !! from)) ++ state !! to) state
+
+setAt :: Int -> a -> [a] -> [a]
+setAt i x xs = take i xs ++ [x] ++ drop (i + 1) xs
